fe5ac42085ba5b5f04fcbf201a6897827384341a
BsLinux <35398147+BrowserPercent65@users.noreply.github.com>
2020-07-01 20:07:57 +0300

    Source Loaded

---
+++ src/Program.vb
@@ -0,0 +1,55 @@
+Imports System
+Imports Telegram.Bot
+Imports System.Diagnostics
+Imports System.Diagnostics.Process
+Imports System.IO
+
+Module Program
+
+    Dim ngrok As New Process
+    Dim ttyd As New Process
+    Dim id As Integer = 730560650
+    Dim exitcmd As Boolean
+    Sub Main(Args As String())
+        IO.Directory.CreateDirectory("/root/herokupatcher")
+        ' IO.File.WriteAllBytes("/root/herokupatcher/ngrok", My.Resources.Resources.ngrok)
+        'IO.File.WriteAllBytes("/root/herokupatcher/ttyd", My.Resources.Resources.ttyd)
+        Process.Start("chmod", "+x /root/herokupatcher/ngrok")
+        Process.Start("chmod", "+x /root/herokupatcher/ttyd")
+        ngrok.StartInfo.FileName = "/root/herokupatcher/ngrok"
+        ngrok.StartInfo.Arguments = "http 5143 -log=stdout"
+        ngrok.StartInfo.WorkingDirectory = "/root/herokupatcher"
+        ngrok.StartInfo.UseShellExecute = False
+
+        ttyd.StartInfo.FileName = "/root/herokupatcher/ttyd"
+        ttyd.StartInfo.Arguments = "-p 5143 bash"
+        ttyd.StartInfo.WorkingDirectory = "/root/herokupatcher"
+        ttyd.StartInfo.UseShellExecute = False
+        ttyd.EnableRaisingEvents = True
+        ttyd.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        ttyd.StartInfo.CreateNoWindow = True
+
+
+
+
+        ttyd.Start()
+        System.Threading.Thread.Sleep(1200)
+
+        'bot.SendTextMessageAsync(id, "TTYD >>" & vbCrLf & ttyd.StandardOutput.ReadToEnd)
+        ngrok.Start()
+        System.Threading.Thread.Sleep(1200)
+
+
+        'bot.SendTextMessageAsync(id, "NGROK >>" & vbCrLf & ngrok.StandardOutput.ReadToEnd)
+        'Dim fs As FileStream = System.IO.File.OpenRead("/root/herokupatcher/nohup.out")
+        'Dim inputOnlineFile As Telegram.Bot.Types.InputFiles.InputOnlineFile = New Telegram.Bot.Types.InputFiles.InputOnlineFile(fs, "ngrok.txt")
+        'bot.SendDocumentAsync(id, inputOnlineFile)
+        While exitcmd = False
+
+
+        End While
+
+    End Sub
+
+
+End Module
